ed44e0075272fb20d51337351c807bde642e1984
Natalia Barkhatova <natabarkhatova@yandex.ru>
2024-08-04 17:43:19 +0300

    feat: change error response

--- src/main/java/ru/barkhatnat/income_tracking/exception_handling/BadRequestControllerAdvice.java
+++ src/main/java/ru/barkhatnat/income_tracking/exception_handling/BadRequestControllerAdvice.java
@@ -6,12 +6,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.barkhatnat.income_tracking.exception.UserAlreadyExistsException;
 
+import java.util.List;
 import java.util.Locale;
 
 @ControllerAdvice
@@ -19,27 +18,24 @@ import java.util.Locale;
 public class BadRequestControllerAdvice {
     private final MessageSource messageSource;
 
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException.class)
     public ResponseEntity<ProblemDetail> handleBindException(BindException e, Locale locale) {
+        List<String> errors = e.getAllErrors().stream()
+                .map(error -> messageSource.getMessage(error, locale))
+                .toList();
+
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,
-                this.messageSource.getMessage(e.getMessage(), new Object[0],
-                        e.getMessage(), locale));
-        problemDetail.setProperty("error",
-                e.getAllErrors().stream()
-                        .map(ObjectError::getDefaultMessage)
-                        .toList());
+                messageSource.getMessage("validation.error.message", null, locale) + " " +
+                        messageSource.getMessage("validation.error.details", null, locale));
+
+        problemDetail.setProperty("error", errors);
         return ResponseEntity.badRequest().body(problemDetail);
     }
 
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<ProblemDetail> handleUserAlreadyExistsException(UserAlreadyExistsException e, Locale locale) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
-                HttpStatus.BAD_REQUEST,
-                messageSource.getMessage(e.getMessage(), new Object[0], e.getMessage(), locale)
-        );
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
+        String message = messageSource.getMessage(e.getMessage(), new Object[0], e.getMessage(), locale);
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, message);
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(problemDetail);
     }
-
 }
