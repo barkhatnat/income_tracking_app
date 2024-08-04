869ea027450a22ab3908c450497d13b079fd6534
Natalia Barkhatova <natabarkhatova@yandex.ru>
2024-08-04 22:05:08 +0300

    feat: delete mapper methods with no usages

--- src/main/java/ru/barkhatnat/income_tracking/utils/CategoryMapper.java
+++ src/main/java/ru/barkhatnat/income_tracking/utils/CategoryMapper.java
@@ -2,16 +2,13 @@ package ru.barkhatnat.income_tracking.utils;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
-import ru.barkhatnat.income_tracking.DTO.CategoryDto;
 import ru.barkhatnat.income_tracking.DTO.CategoryResponseDto;
 import ru.barkhatnat.income_tracking.entity.Category;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface CategoryMapper {
-    CategoryDto toCategoryDto(Category category);
-    CategoryResponseDto toCategoryResponseDto(Category category);
 
-    Iterable<CategoryDto> toCategoryDtoCollection(Iterable<Category> categories);
+    CategoryResponseDto toCategoryResponseDto(Category category);
 
     Iterable<CategoryResponseDto> toCategoryResponseDtoCollection(Iterable<Category> categories);
 }
