5b06d4ebb290268a009eaa83e77ffaa8327cedde
Natalia Barkhatova <natabarkhatova@yandex.ru>
2024-08-04 17:42:07 +0300

    feat: add throwing an exception

--- src/main/java/ru/barkhatnat/income_tracking/controller/AccountRestController.java
+++ src/main/java/ru/barkhatnat/income_tracking/controller/AccountRestController.java
@@ -2,9 +2,6 @@ package ru.barkhatnat.income_tracking.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.MessageSource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindException;
@@ -13,12 +10,11 @@ import org.springframework.web.bind.annotation.*;
 import ru.barkhatnat.income_tracking.DTO.AccountDto;
 import ru.barkhatnat.income_tracking.DTO.AccountResponseDto;
 import ru.barkhatnat.income_tracking.entity.Account;
+import ru.barkhatnat.income_tracking.exception.AccountNotFoundException;
 import ru.barkhatnat.income_tracking.service.AccountService;
 import ru.barkhatnat.income_tracking.utils.AccountMapper;
 import ru.barkhatnat.income_tracking.utils.SecurityUtil;
 
-import java.util.Locale;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -28,14 +24,13 @@ import java.util.UUID;
 @RequestMapping("/accounts/{accountId:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}")
 public class AccountRestController {
     private final AccountService accountService;
-    private final MessageSource messageSource;
     private final AccountMapper accountMapper;
     private final SecurityUtil securityUtil;
 
     @GetMapping
     public ResponseEntity<AccountResponseDto> getAccount(@PathVariable("accountId") UUID accountId) {
         Optional<Account> account = accountService.findAccount(accountId);
-        return account.map(value -> ResponseEntity.ok(accountMapper.toAccountResponseDto(value))).orElseGet(() -> ResponseEntity.notFound().build());
+        return account.map(value -> ResponseEntity.ok(accountMapper.toAccountResponseDto(value))).orElseThrow(() -> new AccountNotFoundException(accountId));
     }
 
     @PatchMapping
@@ -59,13 +54,5 @@ public class AccountRestController {
         UUID currentUserId = securityUtil.getCurrentUserDetails().getUserId();
         this.accountService.deleteAccount(accountId, currentUserId);
         return ResponseEntity.noContent().build();
-    }
-
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<ProblemDetail> handleNoSuchElementException(NoSuchElementException e, Locale locale) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,
-                        this.messageSource.getMessage(e.getMessage(), new Object[0],
-                                e.getMessage(), locale)));
     }
 }
