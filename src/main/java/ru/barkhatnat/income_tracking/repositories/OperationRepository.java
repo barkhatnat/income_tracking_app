6c6eb03fef9654e30b7a42035a861dc3ace02063
Natalia Barkhatova <natabarkhatova@yandex.ru>
2024-08-04 23:05:38 +0300

    feat: add replacing deleted category with "Unknown" category

--- src/main/java/ru/barkhatnat/income_tracking/repositories/OperationRepository.java
+++ src/main/java/ru/barkhatnat/income_tracking/repositories/OperationRepository.java
@@ -1,7 +1,11 @@
 package ru.barkhatnat.income_tracking.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import ru.barkhatnat.income_tracking.entity.Category;
 import ru.barkhatnat.income_tracking.entity.Operation;
 
 import java.util.List;
@@ -10,4 +14,8 @@ import java.util.UUID;
 @Repository
 public interface OperationRepository extends JpaRepository<Operation, UUID> {
     List<Operation> findOperationsByAccountId(UUID account_id);
+
+    @Modifying
+    @Query("UPDATE Operation o SET o.category = :defaultCategory WHERE o.category = :oldCategory")
+    void updateCategory(@Param("oldCategory") Category oldCategory, @Param("defaultCategory") Category defaultCategory);
 }
