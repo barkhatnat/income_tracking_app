e59332f35c3292378e359d0594f19c87bab5fcf9
Natalia Barkhatova <natabarkhatova@yandex.ru>
2024-08-09 16:36:41 +0300

    feat: update OperationUpdatedEvent

--- src/main/java/ru/barkhatnat/income_tracking/event/OperationUpdatedEvent.java
+++ src/main/java/ru/barkhatnat/income_tracking/event/OperationUpdatedEvent.java
@@ -4,14 +4,16 @@ import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 import ru.barkhatnat.income_tracking.entity.Operation;
 
+import java.math.BigDecimal;
+
 @Getter
 public class OperationUpdatedEvent extends ApplicationEvent {
-    private final Operation oldOperation;
     private final Operation newOperation;
+    private final BigDecimal difference;
 
-    public OperationUpdatedEvent(Object source, Operation oldOperation, Operation newOperation) {
+    public OperationUpdatedEvent(Object source, Operation newOperation, BigDecimal difference) {
         super(source);
-        this.oldOperation = oldOperation;
+        this.difference = difference;
         this.newOperation = newOperation;
     }
 }
