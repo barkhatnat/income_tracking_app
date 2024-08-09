f65b9cd44b61dbeae25d4c2e72275a4d59d76376
Natalia Barkhatova <natabarkhatova@yandex.ru>
2024-08-09 16:38:19 +0300

    feat: add new exception

---
+++ src/main/java/ru/barkhatnat/income_tracking/exception/CategoryTypeException.java
@@ -0,0 +1,7 @@
+package ru.barkhatnat.income_tracking.exception;
+
+public class CategoryTypeException extends RuntimeException {
+    public CategoryTypeException(String message) {
+        super(message);
+    }
+}
