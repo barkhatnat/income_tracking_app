a415b66621b228f6306b33f44a8b7a372483eae5
Natalia Barkhatova <natabarkhatova@yandex.ru>
2024-08-09 16:32:55 +0300

    feat: add update account balance method into repository

--- src/main/java/ru/barkhatnat/income_tracking/repositories/AccountRepository.java
+++ src/main/java/ru/barkhatnat/income_tracking/repositories/AccountRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.barkhatnat.income_tracking.entity.Account;
-import ru.barkhatnat.income_tracking.entity.Category;
 
 import java.math.BigDecimal;
 import java.util.List;
