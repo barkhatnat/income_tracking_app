d243c5647a729c73c3eb00c6d459665715f92989
Natalia Barkhatova <natabarkhatova@yandex.ru>
2024-08-09 16:36:59 +0300

    feat: update EventListener

--- src/main/java/ru/barkhatnat/income_tracking/listener/OperationEventListener.java
+++ src/main/java/ru/barkhatnat/income_tracking/listener/OperationEventListener.java
@@ -3,10 +3,14 @@ package ru.barkhatnat.income_tracking.listener;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import ru.barkhatnat.income_tracking.entity.Account;
+import ru.barkhatnat.income_tracking.entity.Operation;
 import ru.barkhatnat.income_tracking.event.OperationCreatedEvent;
 import ru.barkhatnat.income_tracking.event.OperationDeletedEvent;
 import ru.barkhatnat.income_tracking.event.OperationUpdatedEvent;
 import ru.barkhatnat.income_tracking.service.BalanceService;
+
+import java.math.BigDecimal;
 
 @Component
 @RequiredArgsConstructor
@@ -15,17 +19,24 @@ public class OperationEventListener {
 
     @EventListener
     public void handleOperationCreated(OperationCreatedEvent event) {
-        balanceService.establishAccountBalance(event.getOperation().getAccount(), event.getOperation());
+        Operation operation = event.getOperation();
+        changeAccountBalance(operation.getAccount(), operation.getAmount(), operation.getCategory().getCategoryType());
     }
 
     @EventListener
     public void handleOperationUpdated(OperationUpdatedEvent event) {
-        balanceService.cancelAccountBalance(event.getOldOperation().getAccount(), event.getOldOperation());
-        balanceService.cancelAccountBalance(event.getNewOperation().getAccount(), event.getNewOperation());
+        Operation newOperation = event.getNewOperation();
+        BigDecimal difference = event.getDifference();
+        changeAccountBalance(newOperation.getAccount(), difference, newOperation.getCategory().getCategoryType());
     }
 
     @EventListener
     public void handleOperationDeleted(OperationDeletedEvent event) {
-        balanceService.cancelAccountBalance(event.getOperation().getAccount(), event.getOperation());
+        Operation operation = event.getOperation();
+        changeAccountBalance(operation.getAccount(), operation.getAmount(), !operation.getCategory().getCategoryType());
+    }
+
+    private void changeAccountBalance(Account account, BigDecimal amount, Boolean categoryType) {
+        balanceService.changeAccountBalanceByDifference(account, amount, categoryType);
     }
 }
